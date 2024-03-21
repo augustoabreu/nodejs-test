f827f2a1576451a7ae9a8f41da373986c95f0880
Augusto Abreu <guto@live.ca>
2024-03-20 23:46:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(`New request ${JSON.stringify(req)}`);
+  console.log("New request", JSON.stringify(req));
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World!');
 });
 
 server.listen(PORT, () => {
-  console.log(`!! Server running at http://localhost:${PORT}/`);
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
